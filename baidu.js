57be0ea9cb8e7fe2622b25769e3fb064a41cd629
TwentyCJ <sweetererer@163.com>
2021-02-18 19:50:43 +0800

    Site updated: 2021-02-18 19:50:41

---
+++ baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?81327e579a879f3071d4a2cedabdbe36";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
